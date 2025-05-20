c8f2b6d65597aef7ba8386ccf2a0a4000e350d14
Tcavalca47 <t.cavalca47@gmail.com>
2025-05-20 17:45:48 -0300

    Add files via upload
    Arquivos iniciais do projeto

---
+++ script.js
@@ -0,0 +1,11 @@
+function contatos(){
+    window.location.replace("contatos.html");
+}
+
+function projetos(){
+    window.location.replace("projetos.html");
+}
+
+function index(){
+    window.location.replace("index.html");
+}
